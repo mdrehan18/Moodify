7bb7727a369d1fd7b2d83ba53efa538dd9eed674
Md Rehan <mdprehan18@gmail.com>
2026-05-04 18:09:54 +0530

    origin updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,30 +2,23 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const app = express();
 
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (
-      !origin ||
-      origin.includes("vercel.app") ||
-      origin.includes("localhost")
-    ) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
+    origin: "https://moodify-hjh1.onrender.com",
 
-const authRoutes = require("./routes/auth.routes");
-const songRoutes = require("./routes/song.routes");
+    credentials: true
+}))
 
-app.use("/api/auth", authRoutes);
-app.use("/api/songs", songRoutes);
+/**
+ * Routes
+ */
+const authRoutes = require("./routes/auth.routes")
+const songRoutes = require("./routes/song.routes")
 
-module.exports = app;
+app.use("/api/auth", authRoutes)
+app.use("/api/songs", songRoutes)
+
+module.exports = app
